a5ea571a8ccececee1421530390fa94446d2a171
Mark Lanett <mark.lanett@gmail.com>
2011-04-17 13:33:09 -0700

    Implement level

--- lib/unilogger/log_file_emitter.rb
+++ lib/unilogger/log_file_emitter.rb
@@ -9,9 +9,9 @@ module Unilogger
         logdev = options.delete("logdev")
         case logdev
         when nil, /stderr/i
-          new( STDERR, :logdev => "STDERR" )
+          new( STDERR, options.merge( :logdev => "STDERR" ) )
         when /stdout/i
-          new( STDOUT, :logdev => "STDOUT" )
+          new( STDOUT, options.merge( :logdev => "STDOUT" ) )
         else
           new( logdev, options.merge( :logdev => logdev ) )
         end
@@ -22,14 +22,17 @@ module Unilogger
     def initialize( logdev, options = {} )
       @options = options
       @logdev  = ::Logger::LogDevice.new( logdev, options )
+      @level   = Builder.level_to_i( options["level"], ::Logger::Severity::DEBUG )
     end
     
     def emit( details, message, options )
-      if options && options.size > 0 then
-        options = options.map { |k,v| [k, v.to_json].join(":") }.join(",")
-        message = "#{message}; #{options}"
+      if details[:pri_num] >= @level then
+        if options && options.size > 0 then
+          options = options.map { |k,v| [k, v.to_json].join(":") }.join(",")
+          message = "#{message}; #{options}"
+        end
+        @logdev.write "#{details[:time].to_i} [#{details[:pid]}:#{details[:fiber]}] #{details[:pri_sym]} #{message}\n"
       end
-      @logdev.write "#{details[:time].to_i} [#{details[:pid]}:#{details[:fiber]}] #{details[:pri_sym]} #{message}\n"
     end
     
     def as_json
